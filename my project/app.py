76daf4547ab3098d87d6b29865286fa9eb9bd205
Amikaa01 <amitkumar798575@gmail.com>
2026-06-07 13:58:15 +0530

    First commit

---
+++ my project/app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Load saved files
+model = pickle.load(open('fish_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+encoder = pickle.load(open('encoder.pkl', 'rb'))
+
+st.title("🐟 Fish Weight Prediction")
+
+species = st.selectbox(
+    "Species",
+    ['Bream','Parkki','Perch','Pike','Roach','Smelt','Whitefish']
+)
+
+length1 = st.number_input("Length1")
+length2 = st.number_input("Length2")
+length3 = st.number_input("Length3")
+height = st.number_input("Height")
+width = st.number_input("Width")
+
+if st.button("Predict Weight"):
+
+    species_encoded = encoder.transform([[species]]).toarray()
+
+    input_df = pd.DataFrame(
+        [[length1, length2, length3, height, width]],
+        columns=['Length1','Length2','Length3','Height','Width']
+    )
+
+    species_df = pd.DataFrame(
+        species_encoded,
+        columns=encoder.get_feature_names_out(['Species'])
+    )
+
+    final_input = pd.concat([input_df, species_df], axis=1)
+
+    final_input_scaled = scaler.transform(final_input)
+
+    prediction = model.predict(final_input_scaled)
+
+    st.success(f"Predicted Fish Weight: {prediction[0]:.2f} grams")
